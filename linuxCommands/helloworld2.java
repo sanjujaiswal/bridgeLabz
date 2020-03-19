1f6666d242aad35e1fb2d49db55b57fc750e799f
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-03-20 00:29:18 +0530

    Hello World

---
+++ linuxCommands/helloworld2.java
@@ -0,0 +1,12 @@
+public class helloworld2{
+						public static void main(String args[]){
+							while(true){
+								System.out.println("Hello to terminal command");
+												try{
+											Thread.sleep(60000);
+											}catch(Exception ex){
+												System.out.println(ex);
+											}
+		}
+	}
+}
