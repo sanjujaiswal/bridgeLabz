8b7bb21a60d1972ca594dbcec8433becaabefdbf
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-03-20 10:05:19 +0530

    View and Browse commands in linux

---
+++ linuxCommands/ViewAndBrowse.sh
@@ -0,0 +1,57 @@
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/bridgelab/linuxCommands (master)
+$ ls -l /linuxCommands/testt
+ls: cannot access '/linuxCommands/testt': No such file or directory
+
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/bridgelab/linuxCommands (master)
+$ ls -l /testt
+ls: cannot access '/testt': No such file or directory
+
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/bridgelab/linuxCommands (master)
+$ ls -l
+total 14022
+-rw-r--r-- 1 HP 197609    1823 Mar 19 18:04 awk.sh
+-rw-r--r-- 1 HP 197609     934 Mar 19 22:44 data.csv
+-rw-r--r-- 1 HP 197609    2309 Mar 19 17:44 Environment.sh
+-rw-r--r-- 1 HP 197609     315 Mar 19 08:52 file.sh
+-rw-r--r-- 1 HP 197609       0 Mar 19 10:29 file2.sh
+-rw-r--r-- 1 HP 197609    3918 Mar 19 23:37 fileManagement.sh
+-rw-r--r-- 1 HP 197609 8260403 Mar 19 22:45 linux_chit_sheet.pdf
+-rw-r--r-- 1 HP 197609 6065809 Mar 19 22:45 linux_problem_sheet.pdf
+-rw-r--r-- 1 HP 197609    7137 Mar 19 17:31 PipeAndGrip.sh
+-rw-r--r-- 1 HP 197609      62 Mar 19 23:10 test.txt
+drwxr-xr-x 1 HP 197609       0 Mar 19 23:21 testt/
+-rw-r--r-- 1 HP 197609    2874 Mar 19 16:53 Viewfile.sh
+
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/bridgelab/linuxCommands (master)
+$ ls -l testt/
+total 0
+
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/bridgelab/linuxCommands (master)
+$ cd ..
+
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/bridgelab (master)
+$ cd ..
+
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop (master)
+$ cd bridgelab/
+
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/bridgelab (master)
+$ cd linuxCommands/test
+bash: cd: linuxCommands/test: No such file or directory
+
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/bridgelab (master)
+$ ls -l linuxCommands/test
+ls: cannot access 'linuxCommands/test': No such file or directory
+
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/bridgelab (master)
+$ ls -l linuxCommands/test/
+ls: cannot access 'linuxCommands/test/': No such file or directory
+
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/bridgelab (master)
+$ ls linuxCommands/test/
+ls: cannot access 'linuxCommands/test/': No such file or directory
+
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/bridgelab (master)
+$ cd linuxCommands/
+
+
